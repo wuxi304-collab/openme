dee52bc4f5bd8d09b3b2479d7e2f1d78e35c8c28
wuxi304-collab <wuxi304@gmail.com>
2026-07-08 01:24:26 +0800

    feat: export viewer matrix module

---
+++ src/viewer-matrix/index.ts
@@ -0,0 +1,2 @@
+export type { ViewerMatrixEntry, ViewerMatrixLevel, ViewerMatrixStats } from "./types";
+export { buildViewerMatrix, buildViewerMatrixEntry, getViewerMatrixEntryByPath, getViewerMatrixStats } from "./matrix";
