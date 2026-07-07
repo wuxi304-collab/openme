a1e02caa243e11b1eae3cf62124e02031cbaf603
wuxi304-collab <wuxi304@gmail.com>
2026-07-07 09:24:55 +0800

    feat: load file summary panel styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import "./file-summary.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
